f97324f3605d9344e56b5d5ea587cef176eb5307
goldenglorys <olusolagloryolamide@gmail.com>
2021-08-20 17:46:33 +0100

    Preparing for v2

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['.kobocredit.com', '178.128.246.93', 'localhost',]
+ALLOWED_HOSTS = ['.kobocredit.com', '178.128.246.93', 'localhost', '.herokuapp.com',]
 
 
 # Application definition
